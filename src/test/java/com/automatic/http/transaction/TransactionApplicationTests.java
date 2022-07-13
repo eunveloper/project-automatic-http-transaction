074ad7e68ec2abaa3c949923d147b0c6ea3f02af
eunveloper <hye_7991@naver.com>
2022-07-14 01:27:07 +0900

    Initial commit

---
+++ src/test/java/com/automatic/http/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automatic.http.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
